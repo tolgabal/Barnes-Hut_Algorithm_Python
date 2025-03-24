adbc793f567262f16f36d70b041e1619c1a6190d
tolgabal <tolgabal752@gmail.com>
2025-03-24 23:14:30 +0300

    Update quadtree.py

--- quadtree.py
+++ quadtree.py
@@ -29,3 +29,45 @@ class Body:
         F = G * self.mass * other.mass / dist**2
         self.fx += F * dx / dist
         self.fy += F * dy / dist
+        
+class Quad:
+    
+    def __init__(self, x, y, size):
+        self.x = x
+        self.y = y
+        self.size = size
+    
+    def contains(self, body):
+        half = self.size/2
+        return (self.x - half <= body.x <= self.x + half and
+                self.y - half <= body.y <= self.y + half)
+    
+    def northwest(self):
+        half = self.size / 2
+        return Quad(self.x - half/2, self.y + half/2, half)
+
+    def northeast(self):
+        half = self.size / 2
+        return Quad(self.x + half/2, self.y + half/2, half)
+    
+    def southwest(self):
+        half = self.size / 2
+        return Quad(self.x - half/2, self.y - half/2, half)
+    
+    def southeast(self):
+        half = self.size / 2
+        return Quad(self.x + half/2, self.y - half/2, half)
+    
+class Node:
+    def __init__(self, quad):
+        self.quad = quad
+        self.body = None
+        self.total_mass = 0
+        self.mass_x = 0
+        self.mass_y = 0
+        self.divided = False
+        
+        self.NW = None
+        self.NE = None
+        self.SW = None
+        self.SE = None
